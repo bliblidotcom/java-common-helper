c1c6a40855224e2a4ff7257d532f3d6bb280a2db
William Surya Setiadi <william.s.setiadi@gdn-commerce.com>
2018-06-26 11:50:06 +0700

    removed duplicate test

--- src/test/java/com/blibli/oss/common/helper/DateHelperTest.java
+++ src/test/java/com/blibli/oss/common/helper/DateHelperTest.java
@@ -294,35 +294,4 @@ public class DateHelperTest {
     assertTrue(StringUtils.isNotBlank(result));
   }
 
-  @Test
-  public void toString_commonDateTimePatterns_returnsFormattedDate() {
-    Date date = new DateTime(2018, 06, 15, 23, 59, 40, 307).toDate();
-
-    String result = DateHelper.toString(date, CommonDateTimePatterns.dd_MM_yy);
-    assertEquals("15-06-18", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MM_yyyy);
-    assertEquals("15-06-2018", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy);
-    assertEquals("15 Jun 2018", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__HH_mm);
-    assertEquals("15 Jun 2018 23:59", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__hh_mm_a);
-    assertEquals("15 Jun 2018 11:59 PM", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__HH_mm_ss);
-    assertEquals("15 Jun 2018 23:59:40", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__hh_mm_ss_a);
-    assertEquals("15 Jun 2018 11:59:40 PM", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__HH_mm_ss_SSS);
-    assertEquals("15 Jun 2018 23:59:40.307", result);
-
-    result = DateHelper.toString(date, CommonDateTimePatterns.dd_MMM_yyyy__hh_mm_ss_SSS_a);
-    assertEquals("15 Jun 2018 11:59:40.307 PM", result);
-  }
 }
